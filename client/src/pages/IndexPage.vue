e43f4419e85a8d96b9b893d0e1c2199901210856
Mart Anthony Salazar <martsalazar0@gmail.com>
2022-12-29 12:11:10 +0800

    Add todays trending component

--- client/src/pages/IndexPage.vue
+++ client/src/pages/IndexPage.vue
@@ -2,6 +2,7 @@
   <q-page>
     <Navbar />
     <HeaderVue />
+    <TodaysTrending />
   </q-page>
 </template>
 
@@ -9,9 +10,10 @@
 import { defineComponent } from "vue";
 import Navbar from "../components/Navbar.vue";
 import HeaderVue from "src/components/index/HeaderSection.vue";
+import TodaysTrending from "src/components/trending/TodaysTrending.vue";
 
 export default defineComponent({
   name: "IndexPage",
-  components: { Navbar, HeaderVue },
+  components: { Navbar, HeaderVue, TodaysTrending },
 });
 </script>
