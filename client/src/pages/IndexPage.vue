299168f07ea1c1ffdcc646246aebcfa8fe5fc1c0
Mart Anthony Salazar <martsalazar0@gmail.com>
2022-12-29 19:16:00 +0800

    Add global css

--- client/src/pages/IndexPage.vue
+++ client/src/pages/IndexPage.vue
@@ -1,8 +1,9 @@
 <template>
-  <q-page>
+  <q-page class="page">
     <Navbar />
     <HeaderVue />
     <TodaysTrending />
+    <PopularItems />
   </q-page>
 </template>
 
@@ -11,9 +12,22 @@ import { defineComponent } from "vue";
 import Navbar from "../components/Navbar.vue";
 import HeaderVue from "src/components/index/HeaderSection.vue";
 import TodaysTrending from "src/components/trending/TodaysTrending.vue";
+import PopularItems from "src/components/popular/PopularItems.vue";
 
 export default defineComponent({
   name: "IndexPage",
-  components: { Navbar, HeaderVue, TodaysTrending },
+  components: { Navbar, HeaderVue, TodaysTrending, PopularItems },
 });
 </script>
+
+<style scoped>
+.page {
+  background: rgb(255, 255, 240);
+  background: linear-gradient(
+    153deg,
+    rgba(255, 255, 240, 1) 0%,
+    rgba(244, 236, 225, 1) 61%,
+    rgba(244, 236, 225, 1) 100%
+  );
+}
+</style>
