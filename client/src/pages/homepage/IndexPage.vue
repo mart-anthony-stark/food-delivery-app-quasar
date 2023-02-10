92af4ed2c91fcf81ee96465d7062ad12c8c00add
Mart Anthony Salazar <martsalazar0@gmail.com>
2023-02-10 11:14:55 +0800

    change directory

---
+++ client/src/pages/homepage/IndexPage.vue
@@ -0,0 +1,38 @@
+<template>
+  <q-page class="page">
+    <Navbar />
+    <HeaderVue />
+    <TodaysTrending />
+    <PopularItems />
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import Navbar from "../../components/Navbar.vue";
+import HeaderVue from "./HeaderSection.vue";
+import TodaysTrending from "./TodaysTrending.vue";
+import PopularItems from "./popular/PopularItems.vue";
+
+export default defineComponent({
+  name: "IndexPage",
+  components: {
+    Navbar,
+    HeaderVue,
+    TodaysTrending,
+    PopularItems
+  },
+});
+</script>
+
+<style scoped>
+.page {
+  background: rgb(255, 255, 240);
+  background: linear-gradient(
+    153deg,
+    rgba(255, 255, 240, 1) 0%,
+    rgba(244, 236, 225, 1) 61%,
+    rgba(244, 236, 225, 1) 100%
+  );
+}
+</style>
